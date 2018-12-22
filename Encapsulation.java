3f9852290d53ebc51e5977bb7378e7b1640c58d6
chao.ma <chao.ma@brilliantaero.com>
2018-12-22 13:30:50 +0800

    封装

---
+++ Encapsulation.java
@@ -0,0 +1,18 @@
+class Encapsulation {
+    private int age = 0;
+
+    public int getAge () {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Encapsulation e = new Encapsulation();
+
+        e.setAge(18);
+        System.out.println(e.getAge());
+    }
+}
